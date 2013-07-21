1003859bcc3610c4b73ab16eddbacaecc9a41664
Colin Wren <colinwrenca@gmail.com>
2013-07-20 18:51:42 -0700

    Refactor and comment for readability

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 var _ = require('lodash');
-var quote;
-var indent;
 
+// Wrap string in single or double quotes based on user input
+var quoteWrap;
+
+// Create indent to specified nest level
+var createIndent;
+
+// Take an object or array and return stringified JSON that is styled based on
+// user input
 module.exports = function(obj, options) {
   options = options || {};
-  var indentSpace = options.indent || '  ';
   var initialIndent = options.initialIndent || 0;
 
-  quote = (options.quote === '"' || options.quote === 'double') ? '"' : '\'';
-  indent = function (nest) {
-    return multStr(indentSpace, nest);
-  }
+  // Create utility functions based on options
+  quoteWrap = wrap((options.quote === '"' || options.quote === 'double') ? '"' : '\'');
+  createIndent = prefix(options.indent || '  ');
 
-  return indent(initialIndent) + processValue(obj, initialIndent);
+  return createIndent(initialIndent) + processValue(obj, initialIndent);
 }
 
-
-function printObj(obj, nest) {
-  var buffer = _.map(obj, function(value, key) {
-    return indent(nest + 1) + key + ': ' + processValue(value, nest + 1);
-  });
-
-  return '{\n' + buffer.join(',\n') + '\n' + indent(nest) + '}';
-}
-
-function printArray(array, nest) {
-  var buffer = _.map(array, function(value) {
-    return indent(nest + 1) + processValue(value, nest + 1);
-  });
-  return '[\n' + buffer.join(',\n') + '\n' + indent(nest) + ']';
-}
-
+// Process value based on its type
 function processValue(value, nest) {
   if (_.isArray(value)) {
     return printArray(value, nest);
@@ -39,13 +28,39 @@ function processValue(value, nest) {
     return printObj(value, nest);
 
   } else if (_.isString(value)) {
-    return quote  + value + quote;
+    return quoteWrap(value);
 
   } else {
     return value;
   }
 }
 
-function multStr(str, num) {
-  return new Array(num + 1).join(str);
+// Stringify object
+function printObj(obj, nest) {
+  var buffer = _.map(obj, function(value, key) {
+    return createIndent(nest + 1) + key + ': ' + processValue(value, nest + 1);
+  });
+
+  return '{\n' + buffer.join(',\n') + '\n' + createIndent(nest) + '}';
+}
+
+// Stringify array
+function printArray(array, nest) {
+  var buffer = _.map(array, function(value) {
+    return createIndent(nest + 1) + processValue(value, nest + 1);
+  });
+
+  return '[\n' + buffer.join(',\n') + '\n' + createIndent(nest) + ']';
+}
+
+function prefix(space) {
+  return function(nest) {
+    return new Array(nest + 1).join(space);
+  }
+}
+
+function wrap(character) {
+  return function(str) {
+    return character + str + character;
+  }
 }
